1261f8b0dfd467aa7e3f4b8b74191fc9b3998e53
arya020595 <arya.rifqi@alodokter.com>
2023-12-09 17:14:02 +0700

    add controller, service, dto, and validation on module users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { UserModule } from './user/user.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, BookmarkModule, UsersModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
